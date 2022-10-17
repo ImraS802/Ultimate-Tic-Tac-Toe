39b3b32f215bdd17a1ae6f40435845fdb3dc319c
imras <imra.s@gmx.de>
2022-10-17 11:48:55 +0200

    Adding project plan for game

---
+++ script.js
@@ -0,0 +1,53 @@
+function init() {
+  // ! Elements
+/*
+  const start = 
+  const scoreDisplay = 
+  const nextGrid = the position of cell chosen determines the next outside grid to play on
+  const cells = 
+  const playerOne = 
+  const playerTwo = 
+*/
+  // ! Variables
+/*
+  let score = 0
+  let winConstellation = [arrays]
+  let currentPlayer =
+  let winner =
+  */
+  // ! Executions
+/* function grid(item) {
+    arrays with numerical values to reperesent condition of cells, function checks values of cell
+  }
+
+  function
+loop through winConstellation
+Checking for cell values (0, 1, 2) with mathematical operators
+Write victory condition to let grid object know that small grid is won
+Memorize win by setting winConstellation to player value
+call function each time a cell is chosen to check if grid is won (true or false) -> alert which player has won on website
+print in DOM
+
+function isChosen() if else, returns true if this grid can be used for cell choosing
+if nextGrid has been won the following player can play in a grid he chooses by himself, 
+set nextGrid to null to indicate free grids 
+
+
+  function draw() {
+    end game in small grid with a draw via a for loop -> alert in message it's a draw
+  }
+
+  function endGame() to end game if all grids have been won, check via if else return true or false
+
+  function finalWinner () for player who has won the most grids
+if score of player 1 is higher than player 2 he wins
+else player two wins
+ */
+// ! Events
+/*
+  smallGridOne.addEventListener('click', handleClick)
+  all 9 grids
+  */
+}
+
+document.addEventListener('DOMContentLoaded', init)
